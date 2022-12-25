c85c2ecc617a9c6165e375e50bbfa718d17c4076
Manuelka0 <lsgtasda851@yandex.ru>
2022-12-25 14:22:22 +0500

    ended laba - 2

---
+++ test_rest_service1/src/main/java/ru/students/test_rest_service1/service/MyModifyService.java
@@ -0,0 +1,7 @@
+package ru.students.test_rest_service1.service;
+import ru.students.test_rest_service1.model.Response;
+
+public interface MyModifyService
+{
+    Response modify(Response response);
+}
